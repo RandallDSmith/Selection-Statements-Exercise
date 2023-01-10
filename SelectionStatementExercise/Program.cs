530179ecb78dff2aec6d24ecafcbd097f03dcc6b
Randall Smith <rdsmith1761@gmail.com>
2023-01-10 07:14:48 -0500

    Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            
+            Console.WriteLine("Try to guess my favorite number");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("To Low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (userInput == favNumber)
+            {
+                Console.WriteLine("Great Guess!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            Console.WriteLine($"Nice try, my favorite number was {favNumber}");
         }
     }
 }
